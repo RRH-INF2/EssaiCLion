496df7117c48af6b1428622fabc49f27da21224c
rene.rentsch <rene.rentsch@hotmail.com>
2020-10-19 11:18:42 +0200

    Modif de main pour prendre en compte func.h

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include "func.h"
 using namespace std;
 
 int main() {
@@ -7,6 +8,8 @@ int main() {
 	cout << "Modif 2" << endl;
 	cout << "Modif 3a" << endl;
 	cout << "Modif 4b" << endl;
-	cout << "Modif 5" << endl
+	cout << "Modif 5" << endl;
+	int n = 12;
+	cout << n << boolalpha << (estPair(n) ? " est pair" : " est impair") << endl;
 	return EXIT_SUCCESS;
 }
