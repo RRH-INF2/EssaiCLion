476b89427e0cae363f9f9cb497cf8a3fd56d2e6e
rene.rentsch <rene.rentsch@hotmail.com>
2020-10-19 11:09:14 +0200

    Oups!

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 	cout << "Modif 1" << endl;
 	cout << "Modif 2" << endl;
-	cout << "Modif 3" << endl;
-	cout << "Modif 4" << endl;
-	cout << "Modif 5" << endl;
+	cout << "Modif 3a" << endl;
+	cout << "Modif 4b" << endl;
+	cout << "Modif 5" << endl
 	return EXIT_SUCCESS;
 }
